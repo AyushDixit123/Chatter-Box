cd4762341bcba600504e2fe6b6ba7044af5ff6b3
AyushDixit123 <132762104+AyushDixit123@users.noreply.github.com>
2024-06-03 19:20:46 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load environment variables from .env file
 dotenv.config();
 console.log(process.env.PORT);
+connectDB();
 
 // Access environment variables
 const PORT = process.env.PORT;
@@ -14,6 +16,6 @@ app.get('/', (req, res) => {
     res.send('api running');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
