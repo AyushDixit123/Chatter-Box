d4ba3128dc54f405258fce3a1f5847f05c6b227c
AyushDixit123 <132762104+AyushDixit123@users.noreply.github.com>
2024-06-05 12:27:16 +0530

    group chat api creates

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -9,11 +9,11 @@ const chatModel= mongoose.Schema(
         chatName: { type: String, trim: true },
         isGroupChat: { type: Boolean, default: false},
         users: [{
-            type: mongoose.Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId, 
             ref: "User"
         }],
         latestMessage: {
-            type: momgoose.Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: "Message"
         },
         groupAdmin: {
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware')
 const router = express.Router();
-const { accessChat }=require("../controllers/chatControllers")
+const { accessChat,fetchChats,createGroupChat,renameGroupChat, addtoGroupChat,removeFromGroup }=require("../controllers/chatControllers")
 //creating and accesing chats
  router.route('/').post(protect,accessChat );
-// router.route('/').get(protect, fetchChats);
+ router.route('/').get(protect, fetchChats);
 
 //creating groups
-
-// router.route('/group').post(protect, createGroupChat);
-// router.route('/rename').put(protect, renameGroupChat);
-// router.route('/groupremove').put(protect, removefromGroup);
-// router.route("/groupadd").put(protect, addtoGroupChat)
+ router.route('/group').post(protect, createGroupChat);
+ router.route('/rename').put(protect, renameGroupChat);
+ router.route('/groupremove').put(protect, removeFromGroup);
+ router.route("/groupadd").put(protect, addtoGroupChat)
 
 module.exports= router;
 
