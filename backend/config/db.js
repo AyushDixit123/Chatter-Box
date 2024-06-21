e7914a3f0072b6cb185e3a790d86b0136d6190a1
AyushDixit123 <132762104+AyushDixit123@users.noreply.github.com>
2024-06-21 18:19:28 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB= async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI,{
+        await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
             connectTimeoutMS: 30000
         })
-        console.log(`connected succesfully:${conn.connection.host}`)
+        
     } catch (error) {
         console.log('error') 
     }
